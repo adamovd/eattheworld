73f73a81123bfbda81f80c986b8e78c86983fdf6
Adam Danielsson <adam@produktionproduktion.se>
2023-12-07 11:54:57 +0100

    feat: add google and sessionprovider

--- app/api/v1/auth/[...nextauth].ts
+++ app/api/v1/auth/[...nextauth].ts
@@ -1,15 +1,19 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
-export const authOptions = {
-  // Configure one or more authentication providers
+const authOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
-    // ...add more providers here
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
   ],
 };
 
-export default NextAuth(authOptions);
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
