0d5a01460f4676e727b2480a751a6145ed15105d
Adam Danielsson <adam@produktionproduktion.se>
2023-12-08 17:42:43 +0100

    feat: added sessionprovider with middleware from next-auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import NextAuthProvider from "@/helpers/providers";
+import NextAuthProvider from "./Context/Providers";
+import { SessionContext } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
