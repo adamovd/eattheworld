d21bc00c61d4ca91e9dca7a52ba258b0c7067722
Adam Danielsson <adam@produktionproduktion.se>
2023-12-12 13:34:16 +0100

    fix: size of buttons

--- app/Components/Auth.tsx
+++ app/Components/Auth.tsx
@@ -5,7 +5,7 @@ export const LoginButton = () => {
   return (
     <button
       onClick={() => signIn()}
-      className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      className="flex w-50 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
     >
       Sign in
     </button>
@@ -15,7 +15,7 @@ export const LogoutButton = () => {
   return (
     <button
       onClick={() => signOut({})}
-      className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      className="flex w-50 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
     >
       Sign out
     </button>
