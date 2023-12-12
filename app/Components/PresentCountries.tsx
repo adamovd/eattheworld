9c4cd1f5b7f425e97c95576a678fdc95dd60d661
Adam Danielsson <adam@produktionproduktion.se>
2023-12-12 20:42:10 +0100

    feat: show added countries on startpage

---
+++ app/Components/PresentCountries.tsx
@@ -0,0 +1,64 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { getAllCountries } from "../Services/countryServices";
+import { Country } from "../Models/dbTypes";
+import Image from "next/image";
+import { format } from "date-fns";
+
+const PresentCountries = () => {
+  const [countries, setCountries] = useState<Country[]>([]);
+  useEffect(() => {
+    getAllCountries().then((response) => {
+      setCountries(response);
+    });
+  }, []);
+  const deleteCountry = (id: string) => {};
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+          Added Countries
+        </h2>
+
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {countries.map((country, index) => (
+            <div key={index} className="group relative">
+              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                <Image
+                  src={country.imageUrl}
+                  alt={country.name}
+                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  width={400}
+                  height={400}
+                />
+              </div>
+              <div className="mt-4 flex-col justify-between">
+                <div>
+                  <h2 className="text-gray-700">
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    {country.name}
+                  </h2>
+                  <p className="mt-1 text-sm text-gray-500">
+                    {country.description}
+                  </p>
+                </div>
+                <small className="text-sm font-medium text-gray-900">
+                  Created:{" "}
+                  {format(
+                    new Date(country.createdAt),
+                    "dd/MM/yyyy - HH:mm"
+                  ).toString()}
+                </small>
+              </div>
+              <button className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-800">
+                Delete
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PresentCountries;
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,27 @@
+"use client";
 import User from "./Components/User";
 import { LoginButton, LogoutButton } from "./Components/Auth";
 import React from "react";
+import PresentCountries from "./Components/PresentCountries";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
 
-export default async function Home() {
+export default function Home() {
+  const { data: session } = useSession();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Homepage</h1>
+      <section className="flex flex-row gap-3 my-4">
+        {session ? <LogoutButton /> : <LoginButton />}
 
-      <LoginButton />
-      <LogoutButton />
-      <h2>Server side</h2>
-
-      <h2>Client side</h2>
+        <Link
+          href="/admin/dashboard"
+          className="flex w-50 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Dashboard
+        </Link>
+      </section>
       <User />
+      <PresentCountries />
     </main>
   );
 }
