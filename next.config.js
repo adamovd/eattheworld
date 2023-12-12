71fa01cce55d2f314c41cb44bad192b4e28bf90a
Adam Danielsson <adam@produktionproduktion.se>
2023-12-12 20:41:30 +0100

    feat: handle all images

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["@prisma/client", "bcrypt"],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
