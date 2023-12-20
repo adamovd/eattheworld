9722796402f56031313c4125a83c317a5ce44de6
Adam Danielsson <adam@produktionproduktion.se>
2023-12-20 14:23:44 +0100

    fix: change import

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "../globals.css";
+import "../Styles/globals.scss";
 import NextAuthProvider from "../Context/Providers";
 import AdminNavbar from "../Components/Nav/AdminNavbar";
 
