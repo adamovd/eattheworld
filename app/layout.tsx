9381ed04c2bb48a6fbe17351e075300e10cc1c62
Adam Danielsson <adam@produktionproduktion.se>
2023-12-20 14:23:09 +0100

    feat: added header and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "./Styles/globals.scss";
 import NextAuthProvider from "./Context/Providers";
+import Navbar from "./Components/Nav/Navbar";
+import { Container } from "./Styles/Components/Containers";
+import Footer from "./Components/Footer";
 
 export const metadata: Metadata = {
   title: "Eat The World",
@@ -13,9 +16,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="h-full bg-white">
-      <body className="h-full">
-        <NextAuthProvider>{children}</NextAuthProvider>
+    <html lang="en" className="h-full">
+      <body className="h-full flex-col justify-evenly">
+        <NextAuthProvider>
+          <Navbar />
+          <Container>{children}</Container>
+          <Footer />
+        </NextAuthProvider>
       </body>
     </html>
   );
