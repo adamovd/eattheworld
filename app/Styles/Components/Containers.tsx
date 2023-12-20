1861d6fd040d3549090e2751ebd44b1a27208bbd
Adam Danielsson <adam@produktionproduktion.se>
2023-12-20 14:25:56 +0100

    feat: styled container

---
+++ app/Styles/Components/Containers.tsx
@@ -0,0 +1,14 @@
+"use client";
+import styled from "styled-components";
+
+export const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 92vw;
+  margin-left: 4vw;
+  border-left: 1px solid var(--Dark);
+  border-right: 1px solid var(--Dark);
+  background-color: var(--Light);
+`;
