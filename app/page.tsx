92c502e72e62c2aa37410c08ed34d9aca7ffb748
Adam Danielsson <adam@produktionproduktion.se>
2023-12-20 15:44:37 +0100

    fix: clear landingpage

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,6 @@ import { useSession } from "next-auth/react";
 export default function Home() {
   const { data: session } = useSession();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <PresentCountries />
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
 }
