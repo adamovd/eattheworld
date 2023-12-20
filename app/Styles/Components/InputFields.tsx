e1b10fd661e6b527c39a6fb1c5a24a3067930f09
Adam Danielsson <adam@produktionproduktion.se>
2023-12-20 14:26:09 +0100

    feat: styled inputfield

---
+++ app/Styles/Components/InputFields.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+interface InputProps {
+  bgColor: string;
+  textColor: string;
+  fontSize: string;
+}
+
+export const InputField = styled.input<InputProps>`
+  display: flex;
+  height: 45px;
+  width: auto;
+  padding: 0.5625rem 0.625rem 0.375rem 0.9375rem;
+  align-items: flex-start;
+  gap: 0.625rem;
+  border-radius: 1.25rem;
+  background: var(${(props: InputProps) => props.bgColor || "--Light"});
+  color: var(${(props: InputProps) => props.textColor || "--Dark"});
+  font-family: var(--Heading);
+  font-size: ${(props: InputProps) => props.fontSize || "1rem"};
+  border: 1px solid var(--Dark);
+  box-shadow: 2px 4px 0px 0px var(--Dark);
+  &:focus {
+    border: 2px solid var(--Green);
+  }
+`;
