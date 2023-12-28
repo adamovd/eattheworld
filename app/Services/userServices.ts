9a0ae0c828e171d5fb49fb90affb83d8eec1f1aa
Adam Danielsson <adam@produktionproduktion.se>
2023-12-28 19:22:15 +0100

    feat: add country to user

--- app/Services/userServices.ts
+++ app/Services/userServices.ts
@@ -1,4 +1,4 @@
-import { User } from "../Models/dbTypes";
+import { Country, User } from "../Models/dbTypes";
 
 const url =
   process.env.NODE_ENV === "development"
@@ -19,4 +19,28 @@ export const getAllUsers = async () => {
   });
 
   return response.json();
+};
+
+export const addCountryToUser = async (userId: string, countryId: string) => {
+  const url =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/api/v1/users"
+      : "https://www.eattheworld.se/api/v1/users";
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ userId, countryId }),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    }
+
+    throw new Error("Failed to associate country with user");
+  } catch (error) {
+    throw new Error(`Could not associate country with user`);
+  }
 };
