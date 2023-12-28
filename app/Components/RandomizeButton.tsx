99fe3209e0216994056a2600ee61eb4bc6aebc8f
Adam Danielsson <adam@produktionproduktion.se>
2023-12-28 19:21:31 +0100

    feat: add country to user in random

--- app/Components/RandomizeButton.tsx
+++ app/Components/RandomizeButton.tsx
@@ -1,20 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { Button } from "../Styles/Components/Buttons";
-import { Country } from "../Models/dbTypes";
 import { getAllCountries } from "../Services/countryServices";
+import { useRouter } from "next/navigation";
+import { Country } from "@prisma/client";
+import { useSession } from "next-auth/react";
+import { addCountryToUser, getAllUsers } from "../Services/userServices";
 
 const RandomizeButton = () => {
   const [countries, setCountries] = useState<Country[]>([]);
   const [country, setCountry] = useState<Country>();
+  const { data: session } = useSession();
+  const router = useRouter();
+
   useEffect(() => {
     getAllCountries().then((response) => {
       setCountries(response);
     });
   }, []);
+
   const randomizeCountry = () => {
     setCountry(countries[Math.floor(Math.random() * countries.length)]);
-    console.log(country?.name);
   };
+
+  useEffect(() => {
+    if (country) {
+      addCountryToUser(session?.user?.id as string, country.id);
+      getAllUsers().then((response) => console.log(response));
+
+      router.push(`/country/${country.id}`);
+    }
+  }, [country, router]);
+
   return (
     <Button
       bgColor="--Yellow"
