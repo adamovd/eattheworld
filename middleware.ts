81f562f86f15048f0d74015b3b005059c402d929
Adam Danielsson <adam@produktionproduktion.se>
2023-12-29 11:27:15 +0100

    fix: comments and spelling

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,8 @@
-// Ref: https://next-auth.js.org/configuration/nextjs#advanced-usage
 import { withAuth, NextRequestWithAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 
 export default withAuth(
-  // `withAuth` augments your `Request` with the user's token.
   function middleware(request: NextRequestWithAuth) {
-    // console.log(request.nextUrl.pathname)
-    // console.log(request.nextauth.token)
-
     if (
       request.nextUrl.pathname.startsWith("/admin") &&
       request.nextauth.token?.role !== "admin"
@@ -22,6 +17,4 @@ export default withAuth(
   }
 );
 
-// Applies next-auth only to matching routes - can be regex
-// Ref: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = { matcher: ["/admin"] };
