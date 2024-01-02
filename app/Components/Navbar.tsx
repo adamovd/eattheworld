62f71bf776f53425f9a0c6efab7ec16d60885643
Adam Danielsson <adam@produktionproduktion.se>
2024-01-02 22:59:57 +0100

    feat: responsive navbar

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -1,45 +1,193 @@
 "use client";
-import { Header } from "@/app/Styles/Components/Header";
+import {
+  NavbarContainer,
+  LeftContainer,
+  RightContainer,
+  NavbarExtendedContainer,
+  NavbarInnerContainer,
+  NavbarLinkContainer,
+  NavbarLink,
+  Logo,
+  OpenLinksButton,
+  NavbarLinkExtended,
+} from "@/app/Styles/Components/Header";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
-import { LogoutButton, LoginButton } from "./Auth";
-import { Button } from "@/app/Styles/Components/Buttons";
-import Logo from "@/public/logo.svg";
+import Image from "next/image";
 import { usePathname } from "next/navigation";
-usePathname;
+import { useState } from "react";
+import { LoginButton } from "./Auth";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import logo from "../../public/logo.png";
 
 const Navbar = () => {
   const { data: session } = useSession();
   const pathname = usePathname();
-  return (
-    <Header>
-      <section>
-        {session?.user?.role === "admin" ? (
-          <Button bgcolor="--Yellow" textcolor="--Dark" fontSize="1rem">
-            <Link href="/admin/dashboard">Dashboard</Link>
-          </Button>
-        ) : (
-          ""
-        )}
-      </section>
-      <section>
-        <h1>eat the world</h1>
-      </section>
+  const [extendNavbar, setExtendNavbar] = useState(false);
 
-      {pathname.includes("/admin") ? (
-        <section>
-          <Link href="/admin/register/country">Add country</Link>
-          <Link href="/admin/register/recipe">Add recipe</Link>
-          {session ? <LogoutButton /> : <LoginButton />}
-        </section>
-      ) : (
-        <section>
-          <Link href="#">About</Link>
-          <Link href="#">Contact</Link>
-          {session ? <LogoutButton /> : <LoginButton />}
-        </section>
-      )}
-    </Header>
+  return (
+    <NavbarContainer extendnavbar={extendNavbar}>
+      <NavbarInnerContainer>
+        <LeftContainer>
+          <Link href="/">
+            <Logo src={logo} alt="Eat the World logo" />
+          </Link>
+        </LeftContainer>
+        <RightContainer>
+          <NavbarLinkContainer>
+            {session?.user?.role === "admin" ? (
+              <>
+                <NavbarLink href="/admin/dashboard"> Dashboard</NavbarLink>
+                <NavbarLink href="/admin/register/country">
+                  {" "}
+                  Add country
+                </NavbarLink>
+                <NavbarLink href="/admin/register/recipe">
+                  Add recipe
+                </NavbarLink>
+                {session ? (
+                  <NavbarLink href={`/user/${session?.user?.id as string}`}>
+                    <Image
+                      style={{
+                        borderRadius: 50,
+                        maxHeight: 50,
+                        objectFit: "cover",
+                      }}
+                      src={session?.user?.image as string}
+                      width={50}
+                      height={50}
+                      alt={session?.user?.firstname as string}
+                    />
+                  </NavbarLink>
+                ) : (
+                  <LoginButton />
+                )}
+              </>
+            ) : (
+              <>
+                <NavbarLink href="/contact"> Contact</NavbarLink>
+                <NavbarLink href="/about"> About</NavbarLink>
+                {session ? (
+                  <NavbarLink href={`/user/${session?.user?.id as string}`}>
+                    <Image
+                      style={{
+                        borderRadius: 50,
+                        maxHeight: 50,
+                        objectFit: "cover",
+                      }}
+                      src={session?.user?.image as string}
+                      width={50}
+                      height={50}
+                      alt={session?.user?.firstname as string}
+                    />
+                  </NavbarLink>
+                ) : (
+                  <LoginButton />
+                )}
+              </>
+            )}
+
+            <OpenLinksButton
+              onClick={() => {
+                setExtendNavbar((curr) => !curr);
+              }}
+            >
+              {extendNavbar ? (
+                <FontAwesomeIcon fontSize="2rem" icon={faXmark} />
+              ) : (
+                <FontAwesomeIcon fontSize="2rem" icon={faBars} />
+              )}
+            </OpenLinksButton>
+          </NavbarLinkContainer>
+        </RightContainer>
+      </NavbarInnerContainer>
+      <NavbarExtendedContainer>
+        {extendNavbar && (
+          <>
+            {session?.user?.role === "admin" ? (
+              <>
+                <NavbarLinkExtended
+                  href="/admin/dashboard"
+                  onClick={() => {
+                    setExtendNavbar((curr) => !curr);
+                  }}
+                >
+                  {" "}
+                  Dashboard
+                </NavbarLinkExtended>
+                <NavbarLinkExtended
+                  href="/admin/register/country"
+                  onClick={() => {
+                    setExtendNavbar((curr) => !curr);
+                  }}
+                >
+                  {" "}
+                  Add country
+                </NavbarLinkExtended>
+                <NavbarLinkExtended
+                  href="/admin/register/recipe"
+                  onClick={() => {
+                    setExtendNavbar((curr) => !curr);
+                  }}
+                >
+                  Add recipe
+                </NavbarLinkExtended>
+                {session ? (
+                  <NavbarLinkExtended
+                    href={`/user/${session?.user?.id as string}`}
+                    onClick={() => {
+                      setExtendNavbar((curr) => !curr);
+                    }}
+                  >
+                    My pages
+                  </NavbarLinkExtended>
+                ) : (
+                  <NavbarLinkExtended href="/sign-in">
+                    Sign in
+                  </NavbarLinkExtended>
+                )}
+              </>
+            ) : (
+              <>
+                <NavbarLinkExtended
+                  href="/contact"
+                  onClick={() => {
+                    setExtendNavbar((curr) => !curr);
+                  }}
+                >
+                  {" "}
+                  Contact
+                </NavbarLinkExtended>
+                <NavbarLinkExtended
+                  href="/about"
+                  onClick={() => {
+                    setExtendNavbar((curr) => !curr);
+                  }}
+                >
+                  {" "}
+                  About
+                </NavbarLinkExtended>
+                {session ? (
+                  <NavbarLinkExtended
+                    href={`/user/${session?.user?.id as string}`}
+                    onClick={() => {
+                      setExtendNavbar((curr) => !curr);
+                    }}
+                  >
+                    My pages
+                  </NavbarLinkExtended>
+                ) : (
+                  <NavbarLinkExtended href="/sign-in">
+                    Sign in
+                  </NavbarLinkExtended>
+                )}
+              </>
+            )}
+          </>
+        )}
+      </NavbarExtendedContainer>
+    </NavbarContainer>
   );
 };
 
--- app/Styles/Components/Header.tsx
+++ app/Styles/Components/Header.tsx
@@ -1,30 +1,107 @@
 import styled from "styled-components";
+import Link from "next/link";
+import Image from "next/image";
+import { devices } from "../breakpoints";
 
-export const Header = styled.header`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-evenly;
+interface NavProps {
+  extendnavbar: boolean;
+}
+
+export const NavbarContainer = styled.nav<NavProps>`
+  width: 100%;
+  height: ${(props: NavProps) => (props.extendnavbar ? "100vh" : "4rem")};
   border-bottom: 1px solid var(--Dark);
   background: var(--Light);
-  height: 4rem;
-  width: 100%;
-  position: fixed;
-  z-index: 999;
-  section {
-    display: flex;
+  display: flex;
+  flex-direction: column;
+
+  @media ${devices.tablet} {
+    height: 4rem;
     flex-direction: row;
-    align-items: center;
-    justify-content: space-around;
-    width: 33%;
-    font-family: var(--Heading);
-    a {
-      font-family: var(--Text);
-      font-size: 0.8rem;
-    }
-    button > a {
-      font-family: var(--Header);
-      font-size: 1rem;
-    }
+  }
+`;
+
+export const LeftContainer = styled.article`
+  flex: 30%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  padding-left: 10px;
+  @media ${devices.tablet} {
+    padding-left: 4vw;
+  }
+`;
+
+export const RightContainer = styled.article`
+  flex: 70%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+export const NavbarInnerContainer = styled.section`
+  width: 100%;
+  height: 4rem;
+  display: flex;
+`;
+
+export const NavbarLinkContainer = styled.section`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+export const NavbarLink = styled(Link)`
+  color: var(--Dark);
+  font-size: 1rem;
+  font-family: var(--Text);
+  text-decoration: none;
+  margin: 10px;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const NavbarLinkExtended = styled(Link)`
+  color: var(--Dark);
+  font-size: 1rem;
+  font-family: var(--Text);
+  text-decoration: none;
+  margin: 10px;
+  margin-bottom: 1px solid var(--Dark);
+`;
+
+export const Logo = styled(Image)`
+  margin: 10px;
+  max-width: 100px;
+  height: auto;
+  @media ${devices.tablet} {
+    max-width: 150px;
+  }
+`;
+
+export const OpenLinksButton = styled.button`
+  width: 70px;
+  height: 50px;
+  background: none;
+  border: none;
+  color: var(--Dark);
+  cursor: pointer;
+  padding-top: 5px;
+
+  @media ${devices.tablet} {
+    display: none;
+  }
+`;
+
+export const NavbarExtendedContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 1px solid var(--Dark);
+
+  @media ${devices.tablet} {
+    display: none;
   }
 `;
