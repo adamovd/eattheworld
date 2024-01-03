02f2fd958f0cd1c80b643ca4944bbddf460fc474
Adam Danielsson <adam@produktionproduktion.se>
2024-01-03 09:47:51 +0100

    feat: responsive footer

--- app/Styles/Components/Footer.tsx
+++ app/Styles/Components/Footer.tsx
@@ -1,25 +1,33 @@
 import styled from "styled-components";
+import { devices } from "../breakpoints";
 
 export const PageFooter = styled.footer`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
   justify-content: space-evenly;
   border-top: 1px solid var(--Dark);
   background: var(--Yellow);
   width: 100%;
-  height: 25rem;
+  height: auto;
+  min-height: 25rem;
+  @media ${devices.tablet} {
+    flex-direction: row;
+  }
   section {
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: space-around;
-    width: 33%;
+    width: 100%;
     font-family: var(--Heading);
     gap: 2rem;
     a {
       font-family: var(--Text);
       font-size: 0.8rem;
+    }
+    @media ${devices.tablet} {
+      width: 33%;
     }
   }
 `;
