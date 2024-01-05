2cfc58091e63ea636be06c6f2f08054e8882f5cf
Adam Danielsson <adam@produktionproduktion.se>
2024-01-05 10:43:23 +0100

    feat: new page

---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+import PageWrapper from "../Components/PageWrapper";
+
+const ContactPage = () => {
+  return (
+    <PageWrapper>
+      <h1>Contact</h1>
+    </PageWrapper>
+  );
+};
+
+export default ContactPage;
