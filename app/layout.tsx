e9e4bae2735fe43385f7c517d38c526ab5765f3f
Adam Danielsson <adam@produktionproduktion.se>
2024-01-09 17:06:51 +0100

    debug: fonts not working

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
-import "./Styles/globals.scss";
-import "@uploadthing/react/styles.css";
+import "@/app/Styles/globals.scss";
 import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 import { ourFileRouter } from "@/app/api/uploadthing/core";
