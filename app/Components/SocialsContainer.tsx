543020e05692e62fbef5baa5efa45441021a4664
Adam Danielsson <adam@produktionproduktion.se>
2024-01-11 15:46:55 +0100

    feat: remove icons

--- app/Components/SocialsContainer.tsx
+++ app/Components/SocialsContainer.tsx
@@ -1,24 +1,12 @@
-import {
-  faFacebook,
-  faInstagram,
-  faXTwitter,
-} from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import React from "react";
 
 const SocialsContainer = () => {
   return (
     <section>
-      <Link href="https://facebook.com">
-        <FontAwesomeIcon icon={faFacebook} /> Facebook
-      </Link>
-      <Link href="https://instagram.com">
-        <FontAwesomeIcon icon={faInstagram} /> Instagram
-      </Link>
-      <Link href="https://twitter.com">
-        <FontAwesomeIcon icon={faXTwitter} /> X
-      </Link>
+      <Link href="https://facebook.com">Facebook</Link>
+      <Link href="https://instagram.com">Instagram</Link>
+      <Link href="https://twitter.com">X</Link>
     </section>
   );
 };
